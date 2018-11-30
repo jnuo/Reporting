e0efe4050543feee358af260f37bbeacf0b88e23
jnuo <can2Gek>
2018-11-30 16:42:59 +0300

    first successful build

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import Reporter
 import datetime
 import timedelta
+import Helper.Data as dt
 
 today = datetime.datetime.now().date()
 yesterday = today - timedelta.Timedelta( days = 1 )
@@ -11,26 +12,4 @@ yesterday = today - timedelta.Timedelta( days = 1 )
 #print(x)
 
 
-# kelimeler = ['onur','busra']
-#
-# for kelime in kelimeler:
-#     if 'ur' in kelime:
-#         print(kelime)
-
-evciler = \
-    [
-        {
-            'name': 'onur',
-            'age': 32
-        },
-        {
-            'name': 'urfa',
-            'age': 4000000
-        },
-        {
-            'name': 'busra',
-            'age': 28
-        }
-    ]
-
-#evcidf = pd.DataFrame(evciler, index)
+dt.go()
