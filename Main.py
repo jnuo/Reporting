739c5eb50ccd007c03c2a29709d6a189bfcb4127
jnuo <can2Gek>
2018-11-30 12:05:56 +0300

    first successful build

--- Main.py
+++ Main.py
@@ -5,8 +5,32 @@ import timedelta
 today = datetime.datetime.now().date()
 yesterday = today - timedelta.Timedelta( days = 1 )
 
-gaReport = Reporter.get_day_report(yesterday)
+#gaReport = Reporter.get_day_report(yesterday)
 
-x = gaReport[0]
-print(x)
+#x = gaReport[0]
+#print(x)
 
+
+# kelimeler = ['onur','busra']
+#
+# for kelime in kelimeler:
+#     if 'ur' in kelime:
+#         print(kelime)
+
+evciler = \
+    [
+        {
+            'name': 'onur',
+            'age': 32
+        },
+        {
+            'name': 'urfa',
+            'age': 4000000
+        },
+        {
+            'name': 'busra',
+            'age': 28
+        }
+    ]
+
+#evcidf = pd.DataFrame(evciler, index)
