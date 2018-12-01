cb14b6ce92df293360796a04ecfe55a52a79a259
Onur <onurovalii@gmail.com>
2018-12-01 10:15:38 +0300

    data class

--- Helper/Data.py
+++ Helper/Data.py
@@ -1,3 +1,62 @@
 
+import pandas as pd
+
+evciler1 = \
+    [
+        {
+            'name': 'onur ovali',
+            'age': 32
+        },
+        {
+            'name': 'busra kabal ovali',
+            'age': 28
+        },
+        {
+            'name': 'buse kabal',
+            'age': 23
+        }
+    ]
+
+evciler2 = {
+    'name': ['onur ovali', 'busra kabal ovali', 'buse kabal'],
+    'age': [32, 28, 23],
+    'nick': ['iri', 'iri', 'bicirik'],
+    'kilo': [95, 68, 56]
+}
+
 def go():
-    print('data')
+    # df = pd.read_csv("evciler.csv")
+    # df = pd.DataFrame(evciler1)
+    df = pd.DataFrame(evciler2)
+
+    rows, columns = df.shape
+
+    print('')
+
+    # print(df)
+    # print(rows)
+    # print(columns)
+    # print(df.head(2))
+    # print(df.tail(2))
+    # print(df[2:5])
+    # print(df[:])
+    # print(df.columns)
+    # print(df.age)
+    # print(df['name'])
+    # print(type(df['age']))
+    # print( df[['name', 'nick']] )
+    # print( df['age'].max() )
+    # print(df['age'].mean())
+    # print(df['age'].std())
+    # print(df.describe())
+    # print(df[df.kilo == df['kilo'].max()])
+    # print(df[['name', 'age']][df.kilo == df['kilo'].max()])
+
+    df.set_index(['age'], inplace=True)
+    print(df.loc[23])
+    df.reset_index(inplace=True)
+    print(df)
+
+    df.set_index(['nick'], inplace=True)
+    print(df)
+    print(df.loc['iri'])
